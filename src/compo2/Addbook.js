3720bb0fd0f35a15d99f87f8475f74e57250dff7
hariom malviya <hariom.malviya@eclerx.com>
2020-10-01 21:02:24 +0530

    style added : updated AddBook page

--- src/compo2/Addbook.js
+++ src/compo2/Addbook.js
@@ -37,8 +37,9 @@ const Addbook = props => {
             <input class="form-control" type="text" name="bookTitle" placeholder="Enter Title" value={book.bookTitle} onChange={handleInputChange} required /><br /><br />
             <input class="form-control" type="text" name="description" placeholder="Enter Description" value={book.description} onChange={handleInputChange} required /><br /><br />
             <input class="form-control" type="text" name="publisher" placeholder="Enter Publisher" value={book.name} onChange={handleInputChange} required /><br /><br />
-            <button class="form-control" className="btn btn-primary">Add Book</button>
+            <button class="form-control" className="btn btn-primary">Add Book</button> <br/><br/>
         </form>
+    
     )
 }
 
