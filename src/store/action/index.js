1dc1b8cacb9b50cb57d349efd0e20d2597ef6793
hariom malviya <hariom.malviya@eclerx.com>
2020-10-01 21:05:22 +0530

    some actions added

--- src/store/action/index.js
+++ src/store/action/index.js
@@ -1,13 +1,36 @@
+
+
+export const ADD_ARTICLE = 'ADD_BOOK';
+export const SET_ARTICLE = 'SET_BOOK';
+export const REMOVE_ARTICLE = 'DELETE_BOOK';
+
+
  export const like = () => {
     return{
         type:'LIKE'
     }
 };
 
+export const set = (book) => {
+    return {
+      type: 'SET_BOOK',
+      book: book,
+    };
+  };
 
-export const add = () => {
+export const add = (book) => {
     return{
-        type:'ADD',
+        type:'ADD_BOOK',
+        book,
         
     }
 };
+
+export const del = (id) => {
+    return{
+        type:'DELETE_BOOK',
+        id:id,
+        
+    }
+};
+
