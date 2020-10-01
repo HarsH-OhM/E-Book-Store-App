1cb39e0354a1663708ffeaecb20f3684aa836884
hariom malviya <hariom.malviya@eclerx.com>
2020-10-01 21:06:27 +0530

    add reducer function updated

--- src/store/reducers/add.js
+++ src/store/reducers/add.js
@@ -1,11 +1,29 @@
+
+import { SET_BOOK, ADD_Book, DELETE_BOOK} from '../action/index';
+
 const addReducer = (state = [], action)=>{
     switch (action.type) {
-        case 'ADD':
+
+        case 'SET_BOOK':
             
-        return state.concat([action.data]);
+            return action.books;
+
+        case 'ADD_BOOK':
             
+        return state.concat([action.book]);
+        // return [action.book, ...state];
+
+        case 'DELETE_BOOK':
+
+        // return state.filter(initialFormState=> initialFormState.id !== action.id)
+
+            return {
+            ...state,
+
+            books:[state.books.filter(books => books.id !== action.id)]    
+    }
     
-        default:
+        default: 
             return state;
     }
 };
